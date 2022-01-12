3087ab47d3128390a0136a003a9814cb21469421
bibmode <vievenavales29@gmail.com>
2022-01-12 09:33:15 +0800

    font family addition

---
+++ common/components/LoginBox.js
@@ -0,0 +1,15 @@
+import { Container } from "@mui/material";
+
+const LoginBox = () => {
+  return (
+    <Container>
+      <h1>Join thousands of learners from around the world</h1>
+      <p>
+        Master web development by making real-life projects. There are multiple
+        paths for you to choose
+      </p>
+    </Container>
+  );
+};
+
+export default LoginBox;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
+import LoginBox from "../common/components/LoginBox";
 
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <LoginBox />
+    </div>
+  );
 }
